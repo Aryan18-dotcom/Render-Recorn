e982d66a9d814e89ff5f8fbc16cc9226c7160921
Aryan Chheda <aryanchheda18@gmail.com>
2026-07-01 19:09:37 +0530

    Updated the build

--- python-backend/build.sh
+++ python-backend/build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# 1. Standard python packages setup
 pip install -r requirements.txt
 
+# 2. Invoke Playwright via the dynamic python context
 python -m playwright install chromium
