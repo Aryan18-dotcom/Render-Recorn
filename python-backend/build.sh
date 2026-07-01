df4f0d12bcbb451a247a5620f7d72a2ca3d1ba04
Aryan Chheda <aryanchheda18@gmail.com>
2026-07-01 18:51:14 +0530

    Updated the build command

--- python-backend/build.sh
+++ python-backend/build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Install standard dependencies package
 pip install -r requirements.txt
 
-# 2. Install ONLY the browser binaries (no root privileges requested)
-python -m playwright install chromium
+python -m playwright install --with-deps chromium
