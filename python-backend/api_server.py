c0b911f6476570cda80660495901e41fa938ee27
Aryan Chheda <aryanchheda18@gmail.com>
2026-07-02 11:59:42 +0530

    Update the CORS ERROR Problem.

--- python-backend/api_server.py
+++ python-backend/api_server.py
@@ -8,10 +8,20 @@ from parser import WebParser
 from extractor import ContentExtractor
 from generator import MarkdownSpecGenerator
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
 
 load_dotenv()
 
+
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://render-recorn.vercel.app"], # Your Vercel domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Basic logger setup
 logger = logging.getLogger("webagent_api")
