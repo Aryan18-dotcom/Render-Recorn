bee3d1c40a2f54a399ba2af22694147663eb75a3
Aryan Chheda <aryanchheda18@gmail.com>
2026-08-16 16:32:09 +0530

    Fixed the Bugs.

--- python-backend/build.sh
+++ python-backend/build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Ensure the browser path is consistent
 export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/src/.local-browsers
 
-# 2. Install Python packages
 pip install -r requirements.txt
-
-# 3. Install Chromium and its Linux system libraries
-python -m playwright install --with-deps chromium
+python -m playwright install chromium
